ea50b034deec2591c1b8f454a9cb0922ab020bf2
2024.4.12 <2024.4.12@DESKTOP-CUTRVDB>
2025-06-30 21:56:29 -0400

    Add project files.

---
+++ BatePapoClient/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace BatePapoClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            txtUser = new TextBox();
+            txtMessage = new TextBox();
+            lstMessages = new ListBox();
+            btnEnviar = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(38, 56);
+            label1.Name = "label1";
+            label1.Size = new Size(47, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Usuário";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(38, 104);
+            label2.Name = "label2";
+            label2.Size = new Size(66, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Mensagem";
+            // 
+            // txtUser
+            // 
+            txtUser.Location = new Point(114, 56);
+            txtUser.Name = "txtUser";
+            txtUser.Size = new Size(100, 23);
+            txtUser.TabIndex = 2;
+            // 
+            // txtMessage
+            // 
+            txtMessage.Location = new Point(114, 96);
+            txtMessage.Name = "txtMessage";
+            txtMessage.Size = new Size(100, 23);
+            txtMessage.TabIndex = 3;
+            // 
+            // lstMessages
+            // 
+            lstMessages.FormattingEnabled = true;
+            lstMessages.ItemHeight = 15;
+            lstMessages.Location = new Point(352, 56);
+            lstMessages.Name = "lstMessages";
+            lstMessages.Size = new Size(299, 334);
+            lstMessages.TabIndex = 4;
+            // 
+            // btnEnviar
+            // 
+            btnEnviar.Location = new Point(114, 150);
+            btnEnviar.Name = "btnEnviar";
+            btnEnviar.Size = new Size(75, 23);
+            btnEnviar.TabIndex = 5;
+            btnEnviar.Text = "Enviar";
+            btnEnviar.UseVisualStyleBackColor = true;
+            btnEnviar.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnEnviar);
+            Controls.Add(lstMessages);
+            Controls.Add(txtMessage);
+            Controls.Add(txtUser);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtUser;
+        private TextBox txtMessage;
+        private ListBox lstMessages;
+        private Button btnEnviar;
+    }
+}
---
+++ BatePapoServer/Hubs/ChatHub.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using MongoDB.Driver;
+
+namespace BatePapoServer.Hubs
+{
+    public class ChatHub : Hub
+    {
+            private readonly IMongoCollection<ChatMessage> _messages;
+            public ChatHub(IMongoDatabase database)
+            {
+                _messages = database.GetCollection<ChatMessage>("messages");
+            }
+
+        public async Task SendMessage(string user, string message)
+        {
+            var chatMessage = new ChatMessage
+            {
+                User = user,
+                Message = message,
+                Timestamp = DateTime.UtcNow
+            };
+            //Persistir no MongoDb
+            await _messages.InsertOneAsync(chatMessage);
+            //Enviar para todos os clientes conectados
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+
+        }
+    }
+   public class ChatMessage
+    {
+        public string User { get; set; }
+        public string Message { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
